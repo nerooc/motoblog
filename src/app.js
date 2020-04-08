b3b62c4a265fb4d61f765180289b27f80e041907
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-08 11:34:29 +0200

    working with aurelia router

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
+import {
+  PLATFORM
+} from "aurelia-framework";
+
+
 export class App {
-  message = 'Hello World!';
+  constructor() {
+    this.message = 'Hello World!';
+  }
+
+  //ROUTER OF THE PAGE
+  configureRouter(config, router) {
+    config.title = 'Testing Router';
+    config.map([{
+        route: '',
+        name: 'home',
+        moduleId: PLATFORM.moduleName('./index'),
+        title: 'Home'
+      },
+      {
+        route: 'about',
+        name: 'about',
+        moduleId: PLATFORM.moduleName('./about'),
+        title: 'About Us'
+      }
+    ]);
+
+    this.router = router;
+  }
+
 }
