0f6dac541522fbf3aa4659e80cf17cb8fe7e553a
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-10 14:03:02 +0200

    implemented basic router to posts

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+import {
+  PLATFORM
+} from "aurelia-framework";
+
+require('bootstrap/dist/css/bootstrap.min.css');
+require('./assets/styles/blog.css');
+
+
 export class App {
+
   message = 'Hello World!';
+
+
+  configureRouter(config, router) {
+    config.title = 'MotoBlog';
+    config.map([{
+      route: '',
+      name: 'home',
+      moduleId: PLATFORM.moduleName('posts/index'),
+      title: 'All Posts'
+    }]);
+
+
+  }
+
 }
