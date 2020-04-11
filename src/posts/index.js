2a6000428dfa9ca2f9a03890632a8c0682b1b059
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-11 09:16:41 +0200

    using auth-service and post service to automize posts

--- src/posts/index.js
+++ src/posts/index.js
@@ -1,5 +1,27 @@
+import {
+  inject
+} from 'aurelia-framework';
+
+import {
+  PostService
+} from '../common/services/post-service';
+
+@inject(PostService)
 export class Index {
-  constructor() {
-    this.message = 'Hello world';
+
+  constructor(PostService) {
+    this.postService = PostService;
   }
+
+  attached() {
+    this.postService.allPostPreviews().then(data => {
+      if (data.errors) {
+        //Handle the errors
+      } else {
+        this.posts = data.posts;
+        console.log(this.posts);
+      }
+    })
+  }
+
 }
