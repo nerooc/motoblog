ea4c4596cb8f97fc32e72ed90b4af0251b98040c
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-12 16:35:40 +0200

    working on authservice and login component

--- src/app.js
+++ src/app.js
@@ -11,18 +11,26 @@ import {
   PostService
 } from './common/services/post-service';
 
+import {
+  AuthService
+} from './common/services/auth-service';
+
 
 require('bootstrap/dist/css/bootstrap.min.css');
 require('./assets/styles/blog.css');
 
-@inject(PostService)
+@inject(AuthService, PostService)
 export class App {
 
-  constructor(PostService) {
+  constructor(AuthService, PostService) {
     this.postService = PostService;
+    this.authService = AuthService;
+
   }
 
   attached() {
+    this.currentUser = this.authService.currentUser;
+
     this.postService.allTags().then(data => {
       this.tags = data.tags;
     }).catch(error => {
