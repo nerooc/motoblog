e360235f5a0440813c121ddb77de812c264866a9
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-12 15:25:55 +0200

    searching by tags and by archives works

--- src/posts/archive-view.js
+++ src/posts/archive-view.js
@@ -1,5 +1,24 @@
+import {
+  inject
+}
+from 'aurelia-framework';
+
+import {
+  PostService
+} from '../common/services/post-service';
+
+@inject(PostService)
 export class ArchiveView {
-  constructor() {
-    this.message = 'Hello world';
+  constructor(PostService) {
+    this.postService = PostService;
+  }
+
+  activate(params) {
+    this.archive = params.archive;
+    this.postService.postsByArchive(this.archive).then(data => {
+      this.posts = data.posts;
+    }).catch(error => {
+      this.error = error.message;
+    });
   }
 }
--- src/posts/tag-view.js
+++ src/posts/tag-view.js
@@ -1,5 +1,24 @@
+import {
+  inject
+}
+from 'aurelia-framework';
+
+import {
+  PostService
+} from '../common/services/post-service';
+
+@inject(PostService)
 export class TagView {
-  constructor() {
-    this.message = 'Hello world';
+  constructor(PostService) {
+    this.postService = PostService;
+  }
+
+  activate(params) {
+    this.tag = params.tag;
+    this.postService.postsByTag(this.tag).then(data => {
+      this.posts = data.posts;
+    }).catch(error => {
+      this.error = error.message;
+    });
   }
 }
